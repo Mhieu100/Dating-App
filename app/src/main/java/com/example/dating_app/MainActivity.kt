9105e004e7e24f61cb2a7cdfb0b814ad1e2f0570
Mhieu100 <hieunguyen201103@gmail.com>
2023-03-20 18:36:36 +0700

    commit

--- app/src/main/java/com/example/dating_app/MainActivity.kt
+++ app/src/main/java/com/example/dating_app/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.dating_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.dating_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.fragment)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
