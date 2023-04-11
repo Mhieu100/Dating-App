aa6af87a7857cff04ccc6692ef0e65657e34133c
mhieu100 <hieunguyen201103@gmail.com>
2023-04-11 10:16:22 +0700

    commit

--- app/src/main/java/com/example/dating_app/adapter/DatingAdapter.kt
+++ app/src/main/java/com/example/dating_app/adapter/DatingAdapter.kt
@@ -3,15 +3,19 @@ package com.example.dating_app.adapter
 import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.dating_app.activity.MessageActivity
 import com.example.dating_app.databinding.ItemUserLayoutBinding
 import com.example.dating_app.model.UserModel
-import java.util.ArrayList
+import com.example.dating_app.ui.DatingFragment
+import com.yuyakaido.android.cardstackview.*
 
-class DatingAdapter(val context: Context, val list: ArrayList<UserModel>) : RecyclerView.Adapter<DatingAdapter.DatingViewHolder>() {
+class DatingAdapter(var context: Context, val list: ArrayList<UserModel>) : RecyclerView.Adapter<DatingAdapter.DatingViewHolder>() {
     inner class DatingViewHolder(val binding: ItemUserLayoutBinding)
         :RecyclerView.ViewHolder(binding.root)
 
@@ -33,6 +37,8 @@ class DatingAdapter(val context: Context, val list: ArrayList<UserModel>) : Recy
             inter.putExtra("userId", list[position].number)
             context.startActivity(inter)
         }
+
+
     }
 
     override fun getItemCount(): Int {
